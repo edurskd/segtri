5f0897bd7dae309bbd8d6a0be2b15d5cb56c7607
edurskd <102674192+edurskd@users.noreply.github.com>
2024-08-02 07:56:42 -0300

    commit

---
+++ style.css/script.js
@@ -0,0 +1,5 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const caixaresultado = document.querySelector(".caixa-resultado");
